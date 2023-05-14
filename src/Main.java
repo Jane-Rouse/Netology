2ccf836421f8a7d7dc4588a20ce965026d7c575f
Jane-Rouse <131209819+Jane-Rouse@users.noreply.github.com>
2023-05-14 16:10:00 +0300

    Changed int to long

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] sales = new int[]{1, 5, 200, 400};
+        long[] sales = new long[]{1, 5, 200, 400};
         SalesManager saleManager = new SalesManager(sales);
-        int max = saleManager.max();
+        long max = saleManager.max();
         System.out.println(max);
     }
 }
