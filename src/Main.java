3c307a1b72d52f81f2ebe661a345935eb8df677f
Jane-Rouse <131209819+Jane-Rouse@users.noreply.github.com>
2023-05-14 16:58:06 +0300

    Add min() and avarage to SalesManager class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] sales = new int[]{1, 5, 200, 400};
+        int[] sales = new int[]{1, 6, 200, 400};
         SalesManager saleManager = new SalesManager(sales);
         int max = saleManager.max();
+        int min = SalesManager.min();
+        int average = SalesManager.average();
+
         System.out.println(max);
+        System.out.println(min);
+        System.out.println(average);
+
+
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,11 +1,11 @@
 public class SalesManager {
-    protected int[] sales;
+    protected static int[] sales;
 
     public SalesManager(int[] sales) {
         this.sales = sales;
     }
 
-    public int max() {
+    public static int max() {
         int max = -1;
         for (int sale : sales) {
             if (sale > max) {
@@ -14,4 +14,23 @@ public class SalesManager {
         }
         return max;
     }
+
+    public static int min() {
+        int min = Integer.MAX_VALUE;
+        for (int sale : sales) {
+            if (min > sale) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public static int average() {
+        int sum = 0;
+        for (int sale : sales) {
+            sum += sale;
+        }
+        return (sum - ((min() + max()))) / (sales.length - 2);
+    }
+
 }
